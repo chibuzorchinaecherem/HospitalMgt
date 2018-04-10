873c6a8f4621cdcc3a2365dce1af36d90b528caf
okorie chinaecherem <30444900+chibuzorchinaecherem@users.noreply.github.com>
2018-04-10 16:35:21 +0100

    Add files via upload

--- admin/change-password.php
+++ admin/change-password.php
@@ -4,12 +4,27 @@ session_start();
 include('include/config.php');
 //include('include/checklogin.php');
 //check_login();
-
+date_default_timezone_set('Asia/Kolkata');// change according timezone
+$currentTime = date( 'd-m-Y h:i:s A', time () );
+if(isset($_POST['submit']))
+{
+$sql=mysqli_query($con,"SELECT password FROM  admin where password='".$_POST['cpass']."' && username='".$_SESSION['login']."'");
+$num=mysqli_fetch_array($sql);
+if($num>0)
+{
+ $con=mysqli_query($con,"update admin set password='".$_POST['npass']."', updationDate='$currentTime' where username='".$_SESSION['login']."'");
+$_SESSION['msg1']="Password Changed Successfully !!";
+}
+else
+{
+$_SESSION['msg1']="Old Password not match !!";
+}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Admin  | Dashboard</title>
+		<title>Admin | change Password</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 		<meta name="apple-mobile-web-app-capable" content="yes">
@@ -30,7 +45,36 @@ include('include/config.php');
 		<link rel="stylesheet" href="assets/css/styles.css">
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
-
+<script type="text/javascript">
+function valid()
+{
+if(document.chngpwd.cpass.value=="")
+{
+alert("Current Password Filed is Empty !!");
+document.chngpwd.cpass.focus();
+return false;
+}
+else if(document.chngpwd.npass.value=="")
+{
+alert("New Password Filed is Empty !!");
+document.chngpwd.npass.focus();
+return false;
+}
+else if(document.chngpwd.cfpass.value=="")
+{
+alert("Confirm Password Filed is Empty !!");
+document.chngpwd.cfpass.focus();
+return false;
+}
+else if(document.chngpwd.npass.value!= document.chngpwd.cfpass.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.chngpwd.cfpass.focus();
+return false;
+}
+return true;
+}
+</script>
 
 	</head>
 	<body>
@@ -39,7 +83,8 @@ include('include/config.php');
 			<div class="app-content">
 				
 						<?php include('include/header.php');?>
-						
+		
+				</header>
 				<!-- end: TOP NAVBAR -->
 				<div class="main-content" >
 					<div class="wrap-content container" id="container">
@@ -47,81 +92,77 @@ include('include/config.php');
 						<section id="page-title">
 							<div class="row">
 								<div class="col-sm-8">
-									<h1 class="mainTitle">Admin | Dashboard</h1>
+									<h1 class="mainTitle">Admin | Change Password</h1>
 																	</div>
 								<ol class="breadcrumb">
 									<li>
 										<span>Admin</span>
 									</li>
 									<li class="active">
-										<span>Dashboard</span>
+										<span>Change Password</span>
 									</li>
 								</ol>
 							</div>
 						</section>
 						<!-- end: PAGE TITLE -->
 						<!-- start: BASIC EXAMPLE -->
-							<div class="container-fluid container-fullw bg-white">
+						<div class="container-fluid container-fullw bg-white">
 							<div class="row">
-								<div class="col-sm-4">
-									<div class="panel panel-white no-radius text-center">
-										<div class="panel-body">
-											<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i> </span>
-											<h2 class="StepTitle">Manage Patients</h2>
-											
-											<p class="links cl-effect-1">
-												<a href="manage-users.php">
-												<?php $result = mysql_query("SELECT * FROM users ");
-$num_rows = mysql_num_rows($result);
-{
-?>
-											Total Patients :<?php echo htmlentities($num_rows);  } ?>		
-												</a>
-											</p>
+								<div class="col-md-12">
+									
+									<div class="row margin-top-30">
+										<div class="col-lg-8 col-md-12">
+											<div class="panel panel-white">
+												<div class="panel-heading">
+													<h5 class="panel-title">Change Password</h5>
+												</div>
+												<div class="panel-body">
+								<p style="color:red;"><?php echo htmlentities($_SESSION['msg1']);?>
+								<?php echo htmlentities($_SESSION['msg1']="");?></p>	
+													<form role="form" name="chngpwd" method="post" onSubmit="return valid();">
+														<div class="form-group">
+															<label for="exampleInputEmail1">
+																Current Password
+															</label>
+							<input type="password" name="cpass" class="form-control"  placeholder="Enter Current Password">
+														</div>
+														<div class="form-group">
+															<label for="exampleInputPassword1">
+																New Password
+															</label>
+					<input type="password" name="npass" class="form-control"  placeholder="New Password">
+														</div>
+														
+<div class="form-group">
+															<label for="exampleInputPassword1">
+																Confirm Password
+															</label>
+									<input type="password" name="cfpass" class="form-control"  placeholder="Confirm Password">
+														</div>
+														
+														
+														
+														<button type="submit" name="submit" class="btn btn-o btn-primary">
+															Submit
+														</button>
+													</form>
+												</div>
+											</div>
 										</div>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="panel panel-white no-radius text-center">
-										<div class="panel-body">
-											<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-users fa-stack-1x fa-inverse"></i> </span>
-											<h2 class="StepTitle">Manage Doctors</h2>
-										
-											<p class="cl-effect-1">
-												<a href="manage-doctors.php">
-												<?php $result1 = mysql_query("SELECT * FROM doctors ");
-$num_rows1 = mysql_num_rows($result1);
-{
-?>
-											Total Doctors :<?php echo htmlentities($num_rows1);  } ?>		
-												</a>
+											
+											</div>
+										</div>
+									<div class="col-lg-12 col-md-12">
+											<div class="panel panel-white">
 												
-											</p>
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="panel panel-white no-radius text-center">
-										<div class="panel-body">
-											<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-terminal fa-stack-1x fa-inverse"></i> </span>
-											<h2 class="StepTitle"> Appointments</h2>
-											
-											<p class="links cl-effect-1">
-												<a href="book-appointment.php">
-													<a href="appointment-history.php">
-												<?php $sql= mysql_query("SELECT * FROM appointment");
-$num_rows2 = mysql_num_rows($sql);
-{
-?>
-											Total Appointments :<?php echo htmlentities($num_rows2);  } ?>	
-												</a>
-												</a>
-											</p>
+												
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
+						<!-- end: BASIC EXAMPLE -->
 			
 					
 					
